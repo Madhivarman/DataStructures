33738cdc3469e2aa32d74f41eb2c4bf180cb2738
Madhivarman <madhivarman451@gmail.com>
2019-10-30 13:46:07 +0530

    Create countandsay.py

---
+++ leetcodeProblems/countandsay.py
@@ -0,0 +1,21 @@
+def countAndSay(self, n: int) -> str:
+        if n <= 1:
+            return "1"
+        prevCountAndSay = self.countAndSay(n - 1)
+        last = None
+        occurrence = None
+        output = ""
+        for i in prevCountAndSay:
+            if last is not None:
+                if i == last:
+                    occurrence += 1
+                else:
+                    output += str(occurrence) + last
+                    occurrence = 1
+            else:
+                occurrence = 1
+            last = i
+        return output if occurrence is None else output + str(occurrence) + last
+    
+  
+result = countAndSay(5) #prints 111221
