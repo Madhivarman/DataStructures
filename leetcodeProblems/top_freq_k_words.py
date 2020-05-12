9c78bf4f64d919da92c6124c034d12f8e8ab9bce
Madhivarman <madhivarman451@gmail.com>
2020-05-12 22:48:26 +0530

    Create top_freq_k_words.py

---
+++ leetcodeProblems/top_freq_k_words.py
@@ -0,0 +1,30 @@
+"""
+  Given a non-empty list of words, return the k most frequent elements.
+  Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency, 
+  then the word with the lower alphabetical order comes first.
+"""
+
+class Solution:
+    def topKFrequent(self, words, k):
+        hashmap = {}
+        
+        for word in words:
+            if word in hashmap: 
+                hashmap[word] += 1
+            else: 
+                hashmap[word] = 1
+                
+        #reverse the count to negative which bigger number
+        #becomes smaller
+        sort = sorted([ (-hashmap[word], word) for word in hashmap ])
+        res = []
+        
+        for freq, val in sort[:k]:
+            res.append(val)
+        
+        return res
+
+words = ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"]
+k = 4
+
+print(Solution().topKFrequent(words, k))
